ecfdebca11ba3f966b90ce2f91b0462510bdf4a5
Mauro Arango <mauricioudea@gmail.com>
2019-03-19 11:03:01 -0500

    Primer acercamiento de Angular a G-Sheets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit  } from '@angular/core';
+import * as $ from 'jquery';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'emailsapp';
+export class AppComponent implements OnInit {
+  
+
+  public ngOnInit()
+  {
+    var $form = $('form#test-form'),
+    url = 'https://script.google.com/macros/s/AKfycbwVpPB4hjcgZw1CaqaGKayhudZ-7x56anwUOijB5hfRaZ93cJow/exec'
+
+          $('#submit-form').on('click', function(e) {
+          e.preventDefault();
+            var jqxhr = $.ajax({
+              url: url,
+              method: "GET",
+              dataType: "json",
+              data: $form.serializeObject()
+            }).success(
+              // do something
+            );
+          })
+              
+
+
+  }
 }
